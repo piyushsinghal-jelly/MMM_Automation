b81df0c9df362fe5d89644408f36b57ad93cddc5
piyushsinghal-jelly <piyushsinghal@MacBookPro.bbrouter>
2026-06-22 23:50:05 +0530

    updated project structure

---
+++ template.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from pathlib import Path
+import logging
+
+# logging configuration
+logging.basicConfig(
+    level = logging.INFO,
+    format = "[%(asctime)s %(levelname)s]: %(message)s"
+)
+
+# project name
+project_name = 'meridian_automation'
+
+# list the files to be created in the project directory
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "Dockerfile",
+    "setup.py",
+    "requirements.txt",
+    "research/research_notebook.ipynb",
+    "templates/index.html",
+    "app.py"
+]
+
+
+# create all the necessary directories and files for project
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok = True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists, skipping file creation.")
+
+
+
