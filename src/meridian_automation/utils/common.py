b553f39d08e4a48215eb09a1b5809efcfe9b0d76
piyushsinghal-jelly <piyush.singhal@jellyfish.com>
2026-06-27 22:24:15 +0530

    update

--- src/meridian_automation/utils/common.py
+++ src/meridian_automation/utils/common.py
@@ -47,6 +47,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     #     logger.error(f"Error occurred while reading yaml file {path_to_yaml}: {e}")
     #     raise ValueError(f"Error occurred while reading yaml file {path_to_yaml}: {e}")
 
+    # Have to check the Below Functionality 
     try :
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
